198ef740c6186349886fbd2f316ba53c46011d3e
Matt Wakefield <matthew.tyler.wakefield@gmail.com>
2018-11-27 08:19:52 -0600

    Added Call Volume

---
+++ callvolume.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(reshape2)
+library(ggplot2)
+
+
+cv<-readRDS('./Data/cv.RDS')
+
+p_years<-c(2012:2017)
+p_fdid<-c('79553')
+
+cv_f<-cv%>%
+  filter(year %in% p_years & 
+           FDID %in% p_fdid &
+           GSM_FLAG == 0
+           )
+
+cv_t1<-table(cv$IN_TYP_DESC, cv$year)
+
+cv_t2<-table(cv$IN_TYP_DESC2, cv$year)
+
+
+
+
+
+
+
+
+
+
+
